869f82cbd588ccef8f748c00f7719c8083d9b07b
Kevin Menard <nirvdrum@gmail.com>
2011-02-25 18:47:23 -0500

    Allow testing in Rails 2.3.10.

--- remarkable_rails/spec/spec_helper.rb
+++ remarkable_rails/spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 
 RAILS_ENV     = 'test'
-RAILS_VERSION = ENV['RAILS_VERSION'] || '2.3.5'
+RAILS_VERSION = ENV['RAILS_VERSION'] || '2.3.10'
 
 # Load Rails
 gem 'activesupport', RAILS_VERSION
